b936d0a8c34bc276d5d3b31277fc72c78315ba96
synw <ansterj@gmail.com>
2019-11-04 07:05:33 +0100

    Export TimeFrame

--- lib/chartflo.dart
+++ lib/chartflo.dart
@@ -2,5 +2,6 @@ library chartflo;
 
 export 'src/models/models.dart';
 export 'src/sparkline.dart';
+export 'src/timeseries/timeframe.dart';
 export 'src/timeseries/timeseries.dart';
 export 'src/types.dart';
